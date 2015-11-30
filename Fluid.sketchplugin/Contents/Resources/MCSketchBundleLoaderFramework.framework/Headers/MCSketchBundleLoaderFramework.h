06b875cda58d7877e85f2326d179808b32a124e5
Matt Curtis <admin@matt-curtis.me>
2015-11-30 16:40:19 -0600

    1.7

---
+++ Fluid.sketchplugin/Contents/Resources/MCSketchBundleLoaderFramework.framework/Headers/MCSketchBundleLoaderFramework.h
@@ -0,0 +1,17 @@
+//
+//  MCSketchBundleLoaderFramework.h
+//  MCSketchBundleLoaderFramework
+//
+//  Created by Matt Curtis on 7/22/15.
+//  Copyright (c) 2015 Matt. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for MCSketchBundleLoaderFramework.
+FOUNDATION_EXPORT double MCSketchBundleLoaderFrameworkVersionNumber;
+
+//! Project version string for MCSketchBundleLoaderFramework.
+FOUNDATION_EXPORT const unsigned char MCSketchBundleLoaderFrameworkVersionString[];
+
+#import "MCSketchBundleLoader.h"
--- Fluid.sketchplugin/Contents/Sketch/commands.js
+++ Fluid.sketchplugin/Contents/Sketch/commands.js
@@ -5,25 +5,37 @@
 //	--------
 
 $.commands.createCommand("toggleToolbarVisiblity", function(ctx){
-	var pluginController = MCSConstraintsPluginController.pluginController_(ctx.command);
+	var pluginController = [MCFluidPluginController pluginController:ctx.plugin pluginCommand:ctx.command];
 
 	pluginController.toggleToolbarVisiblity();
 });
 
 $.commands.createCommand("editConstraints", function(ctx){
-	var pluginController = MCSConstraintsPluginController.pluginController_(ctx.command);
+	var pluginController = [MCFluidPluginController pluginController:ctx.plugin pluginCommand:ctx.command];
 
-	pluginController.showConstriantInspectorUnderMouse();
+	pluginController.showConstraintInspector();
 });
 
 $.commands.createCommand("updateLayout", function(ctx){
-	var pluginController = MCSConstraintsPluginController.pluginController_(ctx.command);
+	var pluginController = [MCFluidPluginController pluginController:ctx.plugin pluginCommand:ctx.command];
 
 	pluginController.updateLayout();
 });
 
 $.commands.createCommand("preview", function(ctx){
-	var pluginController = MCSConstraintsPluginController.pluginController_(ctx.command);
+	var pluginController = [MCFluidPluginController pluginController:ctx.plugin pluginCommand:ctx.command];
 
 	pluginController.preview();
+});
+
+$.commands.createCommand("toggleArtboardSize", function(ctx){
+	var pluginController = [MCFluidPluginController pluginController:ctx.plugin pluginCommand:ctx.command];
+
+	pluginController.showArtboardSizePresetsPopover();
+});
+
+$.commands.createCommand("showSettingsWindow", function(ctx){
+	var pluginController = [MCFluidPluginController pluginController:ctx.plugin pluginCommand:ctx.command];
+
+	pluginController.showSettingsWindow();
 });
